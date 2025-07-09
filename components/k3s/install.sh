39476f9bd7e528669bd2efb8adc72dee375039b5
Rebels130 <getxzooted@gmail.com>
2025-07-09 01:33:26 -0400

    phasing out manifest reboot true due to persistance

--- components/k3s/install.sh
+++ components/k3s/install.sh
@@ -7,6 +7,12 @@
 set -e
 
 
+if [ -f "/usr/local/bin/k3s" ]; then
+    echo "  ---------> K3s is already installed. No changes needed. <---------  "
+    # Exit with 0 for success, no action needed.
+    exit 0
+fi
+
 echo "  ---------> Installing K3s v${INSTALL_K3S_VERSION} with flags for Calico <---------  "
 VERSIONS_FILE="/opt/Hyperion/configs/versions.json"
 INSTALL_K3S_VERSION=$(jq -r '.platform.k3s' "$VERSIONS_FILE")
@@ -18,12 +24,11 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=${INSTALL_K3S_VERSION} INSTAL
     --disable=kube-proxy \
     --write-kubeconfig-mode=644" sh -
 
-echo "  ---------> K3s installation script finished. <---------  "
-
 
 if [ -n "$SUDO_USER" ]; then 
     chown "$SUDO_USER":"$SUDO_USER" /etc/rancher/k3s/k3s.yaml; 
 fi
 
-
 echo "  ---------> k3s Installation Complete. <---------  "
+
+exit 10
