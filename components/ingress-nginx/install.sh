55d42db8fdc18226de0124f86a0b0acd12aecce7
Rebels130 <getxzooted@gmail.com>
2025-07-13 21:53:54 -0400

    added nginx and cert manager

---
+++ components/ingress-nginx/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Component: ingress-nginx
+set -e
+
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+
+echo "  -> Deploying Nginx Ingress Controller..."
+kubectl apply -f /opt/Hyperion/kubernetes/base/ingress-nginx/deploy.yaml
+
+echo "  -> Waiting for Nginx Ingress Controller to become ready..."
+kubectl wait --for=condition=available -n ingress-nginx deployment/ingress-nginx-controller --timeout=300s
+
+echo "  -> Ingress-Nginx component installed successfully."
