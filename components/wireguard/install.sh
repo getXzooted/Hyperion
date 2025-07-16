9c3fbbf148485d6efd12e75eebcfe7f44bbe3c21
Rebels130 <getxzooted@gmail.com>
2025-07-15 22:23:14 -0400

    wireguard pvc kustomization and new deploy and install

--- components/wireguard/install.sh
+++ components/wireguard/install.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+# Component: wireguard
+
+
 set -e
+
+
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
-echo "  -> Deploying WireGuard Server..."
-kubectl apply -f /opt/Hyperion/kubernetes/apps/wireguard/
+
+echo "  -> Deploying WireGuard..."
+# We use -k to apply the entire directory via its kustomization.
+kubectl apply -k /opt/Hyperion/kubernetes/apps/wireguard/
 
 echo "  -> Waiting for WireGuard to become ready..."
-kubectl wait --for=condition=available -n wireguard deployment/wireguard --timeout=180s
+kubectl wait --for=condition=available -n wireguard deployment/wireguard --timeout=300s
 
 echo "  -> WireGuard component installed successfully."
