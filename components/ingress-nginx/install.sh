dd59934472760ac4809fa2f619bda1803e6129d0
Rebels130 <getxzooted@gmail.com>
2025-07-19 22:07:07 -0400

    new wait condition

--- components/ingress-nginx/install.sh
+++ components/ingress-nginx/install.sh
@@ -13,6 +13,9 @@ kubectl apply -f /opt/Hyperion/kubernetes/base/ingress-nginx/deploy.yaml
 
 echo "  ---------> Waiting for Nginx Ingress Controller to become ready <---------  "
 kubectl wait --for=condition=available -n ingress-nginx deployment/ingress-nginx-controller --timeout=300s
-kubectl wait --for=condition=available -n ingress-nginx deployment/ingress-nginx-controller-admission --timeout=180s
+
+echo "  -> Waiting for Ingress admission controller jobs to complete..."
+kubectl wait --for=condition=complete -n ingress-nginx job/ingress-nginx-admission-create --timeout=180s
+kubectl wait --for=condition=complete -n ingress-nginx job/ingress-nginx-admission-patch --timeout=180s
 
 echo "  ---------> Ingress-Nginx component installed successfully. <---------  "
